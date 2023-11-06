1d8ce0f68bc19a624f6104b31eb23ee323ab7bed
CptrAlexis <99977761+SaturnSpace@users.noreply.github.com>
2023-11-06 15:38:47 +0100

    Update ft_isalnum.c

--- libft.a/ft_isalnum.c
+++ libft.a/ft_isalnum.c
@@ -6,15 +6,26 @@
 /*   By: acarpent <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/06 14:53:22 by acarpent          #+#    #+#             */
-/*   Updated: 2023/11/06 14:53:50 by acarpent         ###   ########.fr       */
+/*   Updated: 2023/11/06 15:31:30 by acarpent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-int	ft_isalnum(int c)
+int	ft_isalnum(char c)
 {
-	if (ft_isdigit(c) || ft_isalpha(c))
+	if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')
+		|| (c >= '0' && c <= '9'))
 		return (1);
 	return (0);
 }
+/*
+#include <stdio.h>
+
+int	main(int argc, char **argv)
+{
+	(void)argc;
+
+	ft_isalnum(*argv[1]);
+	printf("%d", ft_isalnum(*argv[1]));
+	return (0);
+}
+*/
