9bbd6d6c64c3af547bf23f1742822b1f0c19b4f3
CptrAlexis <99977761+SaturnSpace@users.noreply.github.com>
2023-11-10 15:17:28 +0100

    Add files via upload

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,30 +6,25 @@
 /*   By: acarpent <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 13:39:38 by acarpent          #+#    #+#             */
-/*   Updated: 2023/11/09 13:56:48 by acarpent         ###   ########.fr       */
+/*   Updated: 2023/11/10 15:16:56 by acarpent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "linbt.h"
+
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
-	size_t	i;
-	size_t	j;
+	size_t	lensrc;
+	size_t	lendst;
 
-	i = 0;
-	j = 0;
-	while (src[i])
-		i++;
-	while (dst[j])
-		j++;
-	if (src == dst)
-		return (i + j);
-	if (dstsize > (i + j))
-	{
-		
-	}
+	lensrc = ft_strlen(src);
+	lendst = ft_strlen(dst);
+	if (lendst == dstsize)
+		return (lendst + lensrc);
+	if (dstsize > (lendst + lensrc));
+		ft_memcpy(dst + dst_len, src, src_len + 1);
 	else
-	{
 	
-	}
-	return (i + j)
+	return (lensrc + lendst)
+
 }
--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: acarpent <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 12:37:29 by acarpent          #+#    #+#             */
-/*   Updated: 2023/11/09 16:21:17 by acarpent         ###   ########.fr       */
+/*   Updated: 2023/11/10 14:45:24 by acarpent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,3 +26,25 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	}
 	return (lensrc);
 }
+/*
+#include <stdio.h>
+#include <bsd/string.h>
+
+int	main(void)
+{
+	char	src[100] = "Bon jour";
+	char	dst[100] = "oui ";
+	char	dst2[100] = "oui ";
+
+	printf("Avant modif : %s\n", dst);
+
+	ft_strlcpy(dst, src, 5);
+
+	printf("Apres modif : %s\n", dst);
+
+	strlcpy(dst2, src, 5);
+	
+	printf("MAN : %s\n", dst2);
+
+	return (0);
+}*/
